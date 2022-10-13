f1b605f3e9b3eb08be944252f6f73f229159d06a
Eurus-J-Zhang <66300568+Eurus-J-Zhang@users.noreply.github.com>
2022-10-13 10:34:32 +0300

    Add files via upload

---
+++ bored/9_T/script.js
@@ -0,0 +1,13 @@
+// Used to log all actions of the user
+// alert("Error!! There seems to be some problems with the system.");
+$("#A").prop("checked", true);
+
+function fn1(){
+    // var rd1 = document.getElementById("A");
+    // var rd2 = document.getElementById("B");
+    // if (rd1.checked == true)
+    window.location="../10_M/index.html";
+    // else if (rd2.checked == true)
+        // window.location="../page5/index.html";
+}
+
