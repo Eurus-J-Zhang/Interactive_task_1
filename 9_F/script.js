87f71d25d99c713c506a267be36895e756be381b
Eurus J Zhang <zhangjy@jyu.fi>
2022-10-27 21:55:32 +0200

    updated

--- 9_F/script.js
+++ 9_F/script.js
@@ -3,10 +3,5 @@
 $("#B").prop("checked", true);
 
 function fn1(){
-    // var rd1 = document.getElementById("A");
-    // var rd2 = document.getElementById("B");
-    // if (rd1.checked == true)
     window.location="../10_M/index.html";
-    // else if (rd2.checked == true)
-        // window.location="../page5/index.html";
 }
--- 9_T/script.js
+++ 9_T/script.js
@@ -3,11 +3,6 @@
 $("#A").prop("checked", true);
 
 function fn1(){
-    // var rd1 = document.getElementById("A");
-    // var rd2 = document.getElementById("B");
-    // if (rd1.checked == true)
     window.location="../10_M/index.html";
-    // else if (rd2.checked == true)
-        // window.location="../page5/index.html";
 }
 
